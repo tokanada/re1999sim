96d1f35d4ed9d1b2fb7f71d39fa1a97b2b551532
tokanada <davidt.3200@gmail.com>
2024-08-06 22:23:14 -0700

    Implement Multithreading

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import warnings
 import matplotlib.pyplot as plt
 import pandas as pd
-from multiprocessing import Manager, Pool, freeze_support
+from multiprocessing import Pool, freeze_support
 
 warnings.filterwarnings('ignore')
 
@@ -10,17 +10,21 @@ six_star_rate = 0.015
 everything_else_rate = 1 - six_star_rate
 rising_rate = 0.025
 
+
 def generate_character_pool():
     character_pool = ["Everything else", "Six Star"]
     return character_pool
+
 
 def generate_weights():
     weights = [everything_else_rate, six_star_rate]
     return weights
 
+
 def single_roll(character_pool, weights):
     roll = random.choices(character_pool, weights=weights)
     return roll
+
 
 def adjust_weights(weights, adjustment):
     old_six_star_rate = weights[1]
@@ -29,7 +33,9 @@ def adjust_weights(weights, adjustment):
     weights[0] = new_else_rate
     weights[1] = new_six_star_rate
 
+
 def do_loop():
+    roll_results = 0
     character_pool = generate_character_pool()
     weights = generate_weights()
     i = 1
@@ -46,6 +52,7 @@ def do_loop():
         i += 1
     return roll_results
 
+
 def multi_roll(cycles, processes):
     iterations = list(range(0, cycles))
 
@@ -53,6 +60,7 @@ def multi_roll(cycles, processes):
         results = pool.starmap(do_loop, [() for i in iterations])
 
     return results
+
 
 def main():
     cycles = int(input("How many pull cycles? "))
@@ -67,6 +75,7 @@ def main():
     plt.xlabel('Pull #')
     plt.show()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     freeze_support()
     main()
